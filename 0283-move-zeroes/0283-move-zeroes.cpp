10f6a0acbc5b7b8cc434772b44bc8822275a2360
Priyanshi3030 <90553305+Priyanshi3030@users.noreply.github.com>
2023-07-07 15:07:08 +0530

    Time: 20 ms (91.86%), Space: 19.2 MB (31.39%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int count=0;
-        vector<int> store;
-        for(int i=0;i<nums.size();i++){
-        if(nums[i]==0)count++;
-        else{
-            store.push_back(nums[i]);
-        }
-        }
-        while(count--){
-            store.push_back(0);
-        }
-         nums=store;
+        int index=0;
+       for(int i=0;i<nums.size();i++){
+       if(nums[i]!=0){
+           nums[index++]=nums[i];
+  
+       }
+       }
+       for(int i=index;i<nums.size();i++) nums[i]=0;
         }
     
 };
