4961af16558a98a2db2ea197db41b3d9c5c3a3e4
Priyanshi3030 <90553305+Priyanshi3030@users.noreply.github.com>
2023-07-08 08:08:17 +0530

    Time: 32 ms (28.2%), Space: 19.2 MB (31.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,7 +4,7 @@ public:
         int index=0;
        for(int i=0;i<nums.size();i++){
        if(nums[i]!=0){
-           nums[index++]=nums[i];
+           swap(nums[i],nums[index++]);
   
        }
        }
