3dc62fcefdb75ef6fec8cc8012dc6ac19f0f4950
Priyanshi3030 <90553305+Priyanshi3030@users.noreply.github.com>
2023-07-25 21:36:02 +0530

    Time: 29 ms (60.81%), Space: 22.3 MB (75.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-      int n = nums.size(); //size of the array.
-    if (n == 1) return nums[0];
-
-    for (int i = 0; i < n; i++) {
-
-        //Check for first index:
-        if (i == 0) {
-            if (nums[i] != nums[i + 1])
-                return nums[i];
-        }
-        //Check for last index:
-        else if (i == n - 1) {
-            if (nums[i] != nums[i - 1])
-                return nums[i];
-        }
-        else {
-            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1])
-                return nums[i];
-        }
+         int ans=0;
+       if(nums.size()==1)return nums[0];
+       for(int i=0;i<nums.size();i++){
+           if(i==0){
+              if(nums[i]!=nums[i+1]){
+                  ans=nums[i];
+                  break;
+              }
+       }
+       else if(i==nums.size()-1){
+           if(nums[i]!=nums[i-1]){
+               ans=nums[i];
+               break;
+           }
+       }
+       else{
+           if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
+               ans=nums[i];
+               break;
+           }
+       }
     }
-
-    // dummy return statement:
-    return -1;
+    return ans;
 }
     
 };
