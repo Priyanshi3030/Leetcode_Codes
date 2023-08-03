56dcbad489638fee75f4c1030ba971033ebb6d31
Priyanshi3030 <90553305+Priyanshi3030@users.noreply.github.com>
2023-08-03 09:07:38 +0530

    Time: 6 ms (87.7%), Space: 14.5 MB (5.37%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,39 @@
 class Solution {
+    private:
+    void bfs(int sr, int sc,vector<vector<int>>& ans, int color,int initial,
+    vector<vector<int>> & visited, vector<int> first,vector<int>second){
+        int n=ans.size(),m=ans[0].size();
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        visited[sr][sc]=1;
+        ans[sr][sc]=color;
+        while(!q.empty()){
+        auto value=q.front();
+        int row=value.first;
+        int col=value.second;
+        q.pop();
+        for(int i=0;i<4;i++){
+          int r=row+first[i];
+          int c=col+second[i];
+          if(r>=0 && r<n && c>=0 && c<m && !visited[r][c] && ans[r][c]==initial){
+              q.push({r,c});
+              visited[r][c]=1;
+              ans[r][c]=color;
+          }  
+        }
+        
+        }
+    }
 public:
-
-void dfs(vector<vector<int>>& image,vector<vector<int>>& ans,int sr,int sc,
-int initial ,int color,vector<int> first,vector<int> second){
-    int n=image.size();
-    int m=image[0].size();
-  ans[sr][sc]=color;
-  for(int i=0;i<4;i++){
-      int row=sr+first[i];
-      int col=sc+second[i];
-      if(row>=0 && row<n && col>=0 && col<m && ans[row][col]==initial && ans[row][col]!=color){
-          dfs(image,ans,row,col,initial,color,first,second);
-      }
-  }
-
-}
-
-
-
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n=image.size();
+        int m=image[0].size();
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        int ini=image[sr][sc];
         vector<vector<int>> ans=image;
         vector<int> first={-1,0,1,0};
-        vector<int> second={0,1,0,-1};
-        int initial=image[sr][sc];
-        dfs(image,ans,sr,sc,initial,color,first,second);
+        vector<int>second={0,1,0,-1};
+        bfs(sr,sc,ans,color,ini,visited,first,second);
         return ans;
     }
 };
