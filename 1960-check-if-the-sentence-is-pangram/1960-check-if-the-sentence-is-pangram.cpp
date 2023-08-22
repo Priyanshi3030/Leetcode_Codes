257270e38e4d696e1049587b9ea7da845742c98d
Priyanshi3030 <90553305+Priyanshi3030@users.noreply.github.com>
2023-08-22 15:28:04 +0530

    Time: 0 ms (100%), Space: 6.4 MB (80.27%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-       unordered_map<int,int> mp;
-       for(auto i : sentence){
-           mp[i]++;
-       }
-       return mp.size()==26;
+        vector<int>vec(26,0);
+        int count=0;
+        for(int i=0;i<sentence.size();i++){
+        if(vec[sentence[i]-'a']==0){
+        vec[sentence[i]-'a']++;
+        count++;
+        }
+        }
+        return count==26;
+        
+
     }
 };
