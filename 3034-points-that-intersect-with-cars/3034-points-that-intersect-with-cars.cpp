3c0f44e8dc0d27d497ee8dfbc0247840f9b447e9
Priyanshi3030 <90553305+Priyanshi3030@users.noreply.github.com>
2023-09-10 11:29:19 +0530

    Time: 14 ms (75%), Space: 24.5 MB (87.5%) - LeetHub

--- 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     int numberOfPoints(vector<vector<int>>& nums) {
-    map<int,int> mp;
-    for(int i=0;i<nums.size();i++){
-        int start=nums[i][0],end=nums[i][1];
-        for(int j=start;j<=end;j++)mp[j]++;
+    int ans = 0;
+    
+    // Sort the intervals based on their starting points
+    sort(nums.begin(), nums.end());
+    
+    for (int i = 0; i < nums.size(); i++) {
+        int currentStart = nums[i][0];
+        int currentEnd = nums[i][1];
+        
+        while (i + 1 < nums.size() && currentEnd >= nums[i + 1][0]) {
+            // There is an overlap, update currentEnd
+            currentEnd = max(currentEnd, nums[i + 1][1]);
+            i++;
+        }
+        
+        ans += (currentEnd - currentStart + 1);
     }
-    return mp.size();
+    
+    return ans;
 }
 
-    
 };
+    
