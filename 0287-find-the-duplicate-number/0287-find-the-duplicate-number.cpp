c042fac94d5607dde4823aff238ace56497869a8
Priyanshi3030 <90553305+Priyanshi3030@users.noreply.github.com>
2023-09-19 17:24:03 +0530

    Time: 117 ms (34.42%), Space: 61.4 MB (30.42%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,7 +4,10 @@ public:
         sort(nums.begin(),nums.end());
         int ans;
         for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1])ans= nums[i];
+            if(nums[i]==nums[i+1]){
+                ans= nums[i];
+                 break;
+            }  
         }
         return ans;
 
