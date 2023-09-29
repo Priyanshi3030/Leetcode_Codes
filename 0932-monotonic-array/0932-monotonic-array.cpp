9d92697ea5d03faf448dffe212168afc35669fe3
Priyanshi Shivhare <90553305+Priyanshi3030@users.noreply.github.com>
2023-09-29 07:59:16 +0530

    Time: 132 ms (26.24%), Space: 96.8 MB (22.05%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -3,15 +3,14 @@ public:
     bool isMonotonic(vector<int>& nums) {
         int n=nums.size();
         int count=1;
+        int count2=1;
         for(int i=0;i<n-1;i++){
         if(nums[i]<=nums[i+1])count++;
+        if(nums[i]==nums[i+1])count2++;
+        else if(nums[i]>nums[i+1])count2++;
         }
-        if(count==n)return true;
-        count=1;
-        for(int i=0;i<n-1;i++){
-            if(nums[i]>=nums[i+1])count++;
-        }
-        if(count==n)return true;
+        
+        if(count==n || count2==n)return true;
         return false;
     }
 };
