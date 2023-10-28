acaa53b71b57943fbd7e058d83757b88b63e5978
Priyanshi Shivhare <90553305+Priyanshi3030@users.noreply.github.com>
2023-10-28 21:16:31 +0530

    Time: 43 ms (68.61%), Space: 55.7 MB (53.71%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -4,14 +4,11 @@ public:
       int count_zero=0;
       int ans=INT_MIN;
       int i=0,j=0;
-      int count_one=0;
       int n=nums.size();
       while(j<n){
       if(nums[j]==0)count_zero++;
-      else count_one++;
       while(count_zero>k && i<=j){
           if(nums[i]==0)count_zero--;
-          if(nums[i]==1)count_one--;
           i++;
       }
       ans=max(ans,j-i+1);
